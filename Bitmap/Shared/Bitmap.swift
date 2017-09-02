92eae086b5712e59339ef83b703a69333a201c01
juliand665 <julian.dunskus@gmail.com>
2017-09-03 01:36:40 +0200

    Added tons of documentation
    maybe too much?

--- Bitmap/Shared/Bitmap.swift
+++ Bitmap/Shared/Bitmap.swift
@@ -15,15 +15,19 @@ public class Bitmap {
 	static let componentSize = MemoryLayout<UInt8>.size
 	static let pixelSize = MemoryLayout<Pixel>.size
 	
-	public private(set) var width, height: Int
+	/// the bitmap's underlying data
 	public private(set) var pixels: [Pixel]
+	public private(set) var width, height: Int
+	
 	private var context: CGContext
 	
-	/// Initialize a bitmap of the specified width and height
-	/// 
-	/// This constructor fails iff `width * height != pixels.count`
-	/// 
-	/// - Parameter pixels: initial pixels of the bitmap
+	/**
+	Initializes a bitmap of the specified width and height
+	
+	This constructor fails iff `width * height != pixels.count`
+	
+	- Parameter pixels: initial pixels of the bitmap
+	*/
 	public init?(width: Int, height: Int, pixels: [Pixel]) {
 		guard pixels.count == width * height else { return nil }
 		self.width = width
@@ -38,7 +42,7 @@ public class Bitmap {
 		                    bitmapInfo: Bitmap.bitmapInfo)!
 	}
 	
-	/// Initialize a bitmap of the specified width and height
+	/// Initializes a bitmap of the specified width and height
 	/// 
 	/// - Parameter base: the color to initially fill the bitmap with; defaults to `Pixel.clear`
 	public convenience init(width: Int, height: Int, filledWith base: Pixel = .clear) {
@@ -67,10 +71,14 @@ public class Bitmap {
 		context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
 	}
 	
-	/// **DO NOT** keep the context around outside this closure. (If you absolutely have to, just make sure this bitmap stays allocated as long as you need its context.)
-	/// 
-	/// - Parameter block: This is your chance to prove yourself! Do whatever you want with the context you're given, just make sure it doesn't outlive this call. God knows what happens if you do anything to it after this bitmap is deallocated.
-	/// - Parameter context: A context referencing the bitmap's underlying data. Anything you do to this context will be reflected in the bitmap automagically.
+	/**
+	Allows you to access the bitmap's data in form of a `CGContext` which you can use to draw shapes and such.
+	
+	**DO NOT** keep the context around outside this closure. (If you absolutely have to, just make sure this bitmap stays allocated as long as you need its context.)
+	
+	- Parameter block: This is your chance to prove yourself! Do whatever you want with the context you're given, just make sure it doesn't outlive this call. God knows what happens if you do anything to it after this bitmap is deallocated.
+	- Parameter context: A context referencing the bitmap's underlying data. Anything you do to this context will be reflected in the bitmap automagically.
+	*/
 	public func withContext<Result>(do block: (_ context: CGContext) -> Result) -> Result {
 		return block(context)
 	}
--- Bitmap/Shared/Pixel.swift
+++ Bitmap/Shared/Pixel.swift
@@ -8,6 +8,7 @@
 import CoreGraphics
 
 public struct Pixel {
+	/// fully transparent
 	public static let clear = Pixel(red: 0, green: 0, blue: 0, alpha: 0)
 	
 	public static let black = Pixel(red:   0, green:   0, blue:   0)
@@ -20,12 +21,40 @@ public struct Pixel {
 	public static let blue    = Pixel(red:   0, green:   0, blue: 255)
 	public static let magenta = Pixel(red: 255, green:   0, blue: 255)
 	
-	public var r, g, b, a: UInt8
+	/// the red component, out of 255
+	public var red: UInt8
+	/// the green component, out of 255
+	public var green: UInt8
+	/// the blue component, out of 255
+	public var blue: UInt8
+	/// the alpha component, out of 255 (255 means fully opaque)
+	public var alpha: UInt8
 	
+	/**
+	Creates a pixel with the specified components, ranging from 0 to 255.
+	
+	- Parameter red: the red component, out of 255
+	- Parameter green: the green component, out of 255
+	- Parameter blue: the blue component, out of 255
+	- Parameter alpha: the alpha component, out of 255 — defaults to 255 (opaque)
+	*/
 	public init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8 = 255) {
-		(r, g, b, a) = (red, green, blue, alpha)
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.alpha = alpha
 	}
 	
+	/**
+	Creates a pixel with the specified floating-point components, ranging from 0 to 1.
+	
+	Arguments will be clamped to `0.0...1.0` before conversion to `UInt8`; go nuts!
+	
+	- Parameter red: the red component, from 0 to 1
+	- Parameter green: the green component, from 0 to 1
+	- Parameter blue: the blue component, from 0 to 1
+	- Parameter alpha: the alpha component, from 0 to 1 — defaults to 1 (opaque)
+	*/
 	public init<F: BinaryFloatingPoint>(red: F, green: F, blue: F, alpha: F = 1) {
 		self.init(red: red.uInt8, green: green.uInt8, blue: blue.uInt8, alpha: alpha.uInt8)
 	}
